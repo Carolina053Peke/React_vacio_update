ada99941d56806c792d57026292ba6ff7885f099
Carolina <80550158+Carolina053Peke@users.noreply.github.com>
2023-06-15 16:22:44 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 
-import Inicio from "./components/header/Inicio"
-import NavBar from "./components/Nav/NavBar"
-import Foro from "./components/form/Formulario"
-import Footer from "./components/footer/Footer"
-import Soporte from "./components/Soporte/Soporte"
-import Hospitales from "./components/hospitales/Hospitales"
-import MarcoLegal from "./components/marcoLegal/marcolegal"
-
-
 function App() {
  
   return (
  <>
- <NavBar />
- <Inicio />
- <MarcoLegal />
- <Hospitales />
- <Foro />
- <Soporte />
- <Footer />
+  <h1>Mi primer pagina web en react</h1>
  </>
   )
 }
